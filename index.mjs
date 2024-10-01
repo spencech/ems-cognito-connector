99aabdc4196281aa1d4b1aad3cb4c42f82e8b4df
Chris Spence <spencech@gmail.com>
2024-10-01 08:50:07 -0400

    prepublish npm package

--- index.mjs
+++ index.mjs
@@ -1,14 +1,35 @@
 import AmazonCognitoIdentity from 'amazon-cognito-identity-js';
-const { AuthenticationDetails, CognitoUserPool, CognitoUser } = AmazonCognitoIdentity;
 import axios from "axios";
+import jwtDecode from "jwt-decode";
 
-export function Authenticator(UserPoolId, ClientId) {
+const { AuthenticationDetails, CognitoUserPool, CognitoUser, CognitoIdToken, CognitoAccessToken, CognitoRefreshToken, CognitoUserSession } = AmazonCognitoIdentity;
+
+export function Authenticator(UserPoolId, ClientId, IdToken, AccessToken, RefreshToken) {
+  const prepopulate = IdToken && AccessToken && RefreshToken;
   this.UserPoolId = UserPoolId;
   this.ClientId = ClientId;
   this.Pool = new CognitoUserPool({ UserPoolId, ClientId });
   this.Username = null;
   this.Details = null;
+  this.RefreshToken = null;
+
+  if(!prepopulate) return;
+  
+  const decoded  = jwtDecode(idToken);
+
+  this.User = new CognitoUser({
+    Username: decoded['cognito:username'],
+    Pool: this.pool
+  });
+
+  const idTokenObj = new CognitoIdToken({ IdToken: idToken });
+  const accessTokenObj = new CognitoAccessToken({ AccessToken: accessToken });
+  const refreshTokenObj = new CognitoRefreshToken({ RefreshToken: refreshToken });
+  const session = new CognitoUserSession({ IdToken: idTokenObj, AccessToken: accessTokenObj, RefreshToken: refreshTokenObj });
+  this.RefreshToken = refreshTokenObj;
+  this.User.setSignInUserSession(session);
 }
+
 
 Authenticator.prototype.setUsername = function(Username) {
   this.Username = Username;
@@ -40,13 +61,14 @@ Authenticator.prototype.submitOtp = async function(ChallengeResponse) {
   return new Promise(async(resolve, reject) => {
       const handler = {
         onSuccess: async (session) => {
+          this.RefreshToken = session.refreshToken;
           resolve({ status: "success", token: session.idToken.jwtToken, session });
         },
         onFailure: (error) => {
           resolve({ status: "failure", error });
         },
         customChallenge: async (challengeParameters) => { 
-          resolve({ user: this.User, challengeParameters });  
+          resolve({ status: "challenge", user: this.User, challengeParameters });  
         }
       };
       this.User.sendCustomChallengeAnswer(ChallengeResponse, handler);
@@ -56,20 +78,59 @@ Authenticator.prototype.submitOtp = async function(ChallengeResponse) {
 Authenticator.prototype.submitPassword = function(Password) {
   this.Details = new AuthenticationDetails({ Username: this.Username, Password});
   return new Promise((resolve, reject) => {
-    authenticateUser(this.User, this.Details, resolve, reject);
-  });
-}
-
-async function authenticateUser(user, details, resolve, reject) {
-    user.authenticateUser(details, {
+    this.User.authenticateUser(this.Details, {
         onSuccess: (session) => { 
+          this.RefreshToken = session.refreshToken;
           resolve({ status: "success", token: session.idToken.jwtToken, session });
         },
         onFailure: (error) => {
           resolve({ status: "failure", error });
         },
         newPasswordRequired: (userAttributes, requiredAttributes) => {
-          resolve({ status: "success",  message: "password-reset" });
+          resolve({ status: "challenge",  message: "password-reset" });
         }
     });
+  });
 }
+
+Authenticator.prototype.changePassword = function(oldPassword, newPassword) {
+  return new Promise(( resolve, reject) => {
+       this.User.changePassword(oldPassword, newPassword, (error, result) => {
+         if(error) resolve({ status: "failure", error });
+         else resolve({ status: "success", user: this.User });
+       })
+    })
+}
+
+Authenticator.prototype.forgotPassword = function() {
+  return new Promise((resolve, reject) => {
+       this.User.forgotPassword({
+         onSuccess: (response) => resolve({ status: "success", user: this.User }),
+         onFailure: (error) => resolve({ status: "error", error })
+       });
+    });
+}
+
+Authenticator.prototype.setNewPasswordWithResetCode = function(code, password) {
+  return new Promise((resolve, reject ) => {
+       this.User.confirmPassword(code, password, {
+         onSuccess: (response) => { resolve({ status: "success", user: this.User}) },
+         onFailure: (error) => resolve({ status: "failure", error })
+       })
+    });
+}
+
+Authenticator.prototype.refreshToken = function() {
+  return new Promise((resolve, reject) => {
+    this.User.refreshSession(this.RefreshToken, (error, session) => {
+      if(error) resolve({ status: "failure", error });
+      else {
+        this.RefreshToken = session.refreshToken;
+        resolve({ status: "success", token: session.idToken.jwtToken, session });
+      }
+    })
+  });
+}
+
+
+
